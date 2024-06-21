a4ce6c1c3ebe39bd8ce115cf8ff2d272b2965769
nikaagokh <ngokhadze99@gmail.com>
2024-06-22 00:04:05 +0400

    faw

--- language-detector.js
+++ language-detector.js
@@ -4,7 +4,9 @@ const LanguageDetector = (req, res, next) => {
     const segments = req.path.split('/');
     if(segments.length > 1 && isLanguageCode(segments[1])) {
         req.language = segments[1];
+        console.log(req.url);
         req.url = req.url.substring(req.url.indexOf('/', req.url.indexOf('/') + 1));
+        console.log(req.url);
     } else {
         req.language = 'en';
     } 
