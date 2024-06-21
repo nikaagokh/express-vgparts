3cc1a5596375cdf507e3e366d874f6a55008f64d
nikaagokh <ngokhadze99@gmail.com>
2024-06-22 00:14:02 +0400

    fa

--- language-detector.js
+++ language-detector.js
@@ -1,16 +1,19 @@
 const definedLanguages = ['en', 'ka'];
 
 const LanguageDetector = (req, res, next) => {
+    console.log(req.path);
     const segments = req.path.split('/');
+    
+    console.log(segments);
     if(segments.length > 1 && isLanguageCode(segments[1])) {
         req.language = segments[1];
-        console.log(req.url);
         req.url = req.url.substring(req.url.indexOf('/', req.url.indexOf('/') + 1));
-        console.log(req.url);
     } else {
+
         req.language = 'en';
+        req.url = '/';
     } 
-    next();
+    //next();
 }
 
 const isLanguageCode = (code) => {
